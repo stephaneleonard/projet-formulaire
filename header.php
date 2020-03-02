b84d5bdd85dbecaaa12b8589d3c4b822ceb19a6e
stephaneleonard <stefleonard97@gmail.com>
2020-03-02 15:32:17 +0100

    layout

---
+++ header.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="./assets/css/style.css">
+    <title>Hackers Poulette</title>
+</head>
+
+<body>
+    <header>
+        <h1>coucou les poulettes</h1>
+    </header>
--- index.php
+++ index.php
@@ -1,12 +1,4 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="./assets/css/style.css"> 
-    <title>Hackers Poulette</title>
-</head>
-<body>
-    <h1>coucou les poulettes</h1>
-</body>
-</html>
+<?php
+include './header.php';
+include './shop.php';
+include './footer.php';
