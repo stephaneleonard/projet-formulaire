0ac0f6c28848e9befb1ec919fc443e60d841c83a
stephaneleonard <stefleonard97@gmail.com>
2020-03-03 14:01:39 +0100

    header and shop v1

--- shop.php
+++ shop.php
@@ -1,7 +1,89 @@
 <main>
-    <section class="featured">
+    <section id="featured">
         <div class="container">
             <h2>Featured Kit</h2>
+            <div class="row">
+                <div class="card mx-auto col-md-4 ">
+                    <img class="card-img-top" src="./assets/img/kits/kit1.jpg" alt="Card image cap">
+                    <div class="card-body d-flex flex-column">
+                        <h5 class="card-title">Raspberry Pi 4 Model B Starter Kit</h5>
+                        <p class="card-text">Our great value Raspberry Pi 4 starter kit is best way to start using the Raspberry Pi 4, and includes everything you need – including high-quality official Raspberry Pi accessories.</p>
+                        <a href="#" class="btn btn-primary mt-auto">Add To Basket 59$</a>
+                    </div>
+                </div>
+                <div class="card mx-auto col-md-4">
+                    <img class="card-img-top" src="./assets/img/kits/kit2.png" alt="Card image cap">
+                    <div class="card-body d-flex flex-column">
+                        <h5 class="card-title">Raspberry Pi 4 Model B Retro Gaming Kit</h5>
+                        <p class="card-text">Our great value Raspberry Pi 4 Retro Gaming Kit is the best way to create your very own retro gaming console and includes everything you need – including high-quality official Raspberry Pi accessories.</p>
+                        <a href="#" class="btn btn-primary mt-auto">Add To Basket 88$</a>
+                    </div>
+                </div>
+                <div class="card mx-auto col-md-4">
+                    <img class="card-img-top" src="./assets/img/kits/kit3.jpg" alt="Card image cap">
+                    <div class="card-body d-flex flex-column">
+                        <h5 class="card-title">Picade - Raspberry Pi Arcade Machine</h5>
+                        <p class="card-text">The ultimate desktop retro arcade machine! Picade is a Raspberry Pi-powered mini arcade that you build yourself, with authentic arcade controls, a high resolution 4:3 display that's ideal for retro gaming, and a punchy speaker to hear those 8-bit game soundtracks at their best!</p>
+                        <a href="#" class="btn btn-primary mt-auto">Add To Basket 100$</a>
+                    </div>
+                </div>
+            </div>
+        </div>
+
+    </section>
+    <section id="gaming">
+        <div class="container">
+            <h2>Gaming</h2>
+            <div class="row">
+                <div class="card mx-auto col-md-4">
+                    <img class="card-img-top" src="./assets/img/kits/kit2.png" alt="Card image cap">
+                    <div class="card-body d-flex flex-column">
+                        <h5 class="card-title">Raspberry Pi 4 Model B Retro Gaming Kit</h5>
+                        <p class="card-text">Our great value Raspberry Pi 4 Retro Gaming Kit is the best way to create your very own retro gaming console and includes everything you need – including high-quality official Raspberry Pi accessories.</p>
+                        <a href="#" class="btn btn-primary mt-auto">Add To Basket 88$</a>
+                    </div>
+                </div>
+                <div class="card mx-auto col-md-4">
+                    <img class="card-img-top" src="./assets/img/kits/kit3.jpg" alt="Card image cap">
+                    <div class="card-body d-flex flex-column">
+                        <h5 class="card-title">Picade - Raspberry Pi Arcade Machine</h5>
+                        <p class="card-text">The ultimate desktop retro arcade machine! Picade is a Raspberry Pi-powered mini arcade that you build yourself, with authentic arcade controls, a high resolution 4:3 display that's ideal for retro gaming, and a punchy speaker to hear those 8-bit game soundtracks at their best!</p>
+                        <a href="#" class="btn btn-primary mt-auto">Add To Basket 100$</a>
+                    </div>
+                </div>
+                <div class="card mx-auto col-md-4">
+                    <img class="card-img-top" src="./assets/img/kits/kit4.jpg" alt="Card image cap">
+                    <div class="card-body d-flex flex-column">
+                        <h5 class="card-title">RetroFlag SUPERPi Case (Deluxe Edition-J)</h5>
+                        <p class="card-text">We created this Pi case applying the most iconic element in the retro game world as a homage to the most classic console ever made.</p>
+                        <a href="#" class="btn btn-primary mt-auto">Add To Basket 30$</a>
+                    </div>
+                </div>
+            </div>
+        </div>
+
+    </section>
+    <section id="cluster">
+        <div class="container">
+            <h2>Cluster</h2>
+            <div class="row">
+                <div class="card mx-auto col-md-4">
+                    <img class="card-img-top" src="./assets/img/kits/kit5.jpg" alt="Card image cap">
+                    <div class="card-body d-flex flex-column">
+                        <h5 class="card-title">Cluster HAT v2.0</h5>
+                        <p class="card-text">The Cluster HAT interfaces a (Controller) Raspberry Pi 4  (or older A+/B+/2/3) with 4 Raspberry Pi Zeros . Configured to use USB Gadget mode, it is an ideal tool for teaching, testing or simulating small scale clusters.</p>
+                        <a href="#" class="btn btn-primary mt-auto">Add To Basket 28$</a>
+                    </div>
+                </div>
+                <div class="card mx-auto col-md-4">
+                    <img class="card-img-top" src="./assets/img/kits/kit6.jpg" alt="Card image cap">
+                    <div class="card-body d-flex flex-column">
+                        <h5 class="card-title">Cluster HAT Case</h5>
+                        <p class="card-text">We've given ModMyPi's previous Cluster HAT case a full upgrade and makeover for the Raspberry Pi 4 Model B (and improved the design of the Raspberry Pi 3 version).</p>
+                        <a href="#" class="btn btn-primary mt-auto">Add To Basket 12$</a>
+                    </div>
+                </div>
+            </div>
         </div>
 
     </section>
